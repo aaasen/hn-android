e597b945d063580088e5b921d50732a2ca8425a5
Lane Aasen <laneaasen@gmail.com>
2013-06-24 22:54:13 -0700

    adds jsoup and real data from hacker news

--- src/com/aaasen/hn/StoriesActivity.java
+++ src/com/aaasen/hn/StoriesActivity.java
@@ -1,10 +1,19 @@
 package com.aaasen.hn;
 
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class StoriesActivity extends Activity {
 
@@ -12,34 +21,35 @@ public class StoriesActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stories);
-
-		final TextView textview = (TextView) findViewById(R.id.textview);
 		
 		Intent intent = getIntent();
 		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-		textview.setText(message);
+		Document doc = Jsoup.parse(message);
 		
+		Elements links = doc.select("tr > td.title > a");
+		ArrayList<Link> stories = new ArrayList<Link>();
 		
-//		final ListView listview = (ListView) findViewById(R.id.listview);
-//		
-//		
-//		
-//		final HNArrayAdapter adapter = new HNArrayAdapter(this, links);
-//
-//		listview.setAdapter(adapter);
-//
-//		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//			@Override
-//			public void onItemClick(AdapterView<?> parent, final View view,
-//					int position, long id) {
-//				Intent intent = new Intent(view.getContext(), CommentsActivity.class);
-//		    	activity.startActivity(intent);
-//			}
-//		});
+		for (Element link : links) {
+			stories.add(new Link(link.attr("href"), "", 123));
+		}
+
+
+		final ListView listview = (ListView) findViewById(R.id.listview);
+
+		final HNArrayAdapter adapter = new HNArrayAdapter(this, stories.toArray(new Link[stories.size()]));
+
+		listview.setAdapter(adapter);
+
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, final View view,
+					int position, long id) {
+				Intent intent = new Intent(view.getContext(), CommentsActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 	
-	
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
