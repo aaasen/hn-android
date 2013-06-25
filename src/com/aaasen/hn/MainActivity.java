e37723d1cbc8e136e5c61f83968a42078d7a1021
Lane Aasen <laneaasen@gmail.com>
2013-06-24 17:10:26 -0700

    adds a simple list of placeholder topics that can be clicked on

--- src/com/aaasen/hn/MainActivity.java
+++ src/com/aaasen/hn/MainActivity.java
@@ -1,8 +1,16 @@
 package com.aaasen.hn;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +18,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final ListView listview = (ListView) findViewById(R.id.listview);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+				"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+				"Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+				"OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+				"Android", "iPhone", "WindowsMobile" };
+
+		final ArrayList<String> list = new ArrayList<String>(Arrays.asList(values));
+
+		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+
+		listview.setAdapter(adapter);
+
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, final View view,
+					int position, long id) {
+				Intent intent = new Intent(view.getContext(), CommentsActivity.class);
+		    	startActivity(intent);
+				
+				final String item = (String) parent.getItemAtPosition(position);
+			}
+		});
 	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
